2dec080f73de750976964a5e47747c1dd2b5a9ac
Brijesh Kumar <bk10895@gmail.com>
2023-09-19 13:57:10 +0530

    feat: upgrade testing library and add tests for ParsedResults component
    - Upgrade @testing-library/react from v13.4.0 to v14.0.0
    - Add unit tests for ParsedResults component to check correct rendering
    - Rename ParseResult to ParsedResults for better clarity
    - Handle empty state in ParsedResults component display

---
+++ src/components/ParseResult/ParsedResults.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { ParsedResults } from "./ParsedResults";
+import { ParsedResult } from "../../types";
+
+describe("ParsedResults", () => {
+  it("renders the list of parsed results correctly", () => {
+    const results: ParsedResult[] = [
+      { lineNumber: 1, address: "address1", amount: 100 },
+      { lineNumber: 2, address: "address2", amount: 200 },
+    ];
+
+    render(<ParsedResults results={results} />);
+
+    // Check if the component renders the correct elements and text
+    expect(screen.getByText("Parsed Results:")).toBeInTheDocument();
+    expect(
+      screen.getByText("Send 100 to address address1")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Send 200 to address address2")
+    ).toBeInTheDocument();
+  });
+
+  it("renders no results message when there are no results", () => {
+    const results: ParsedResult[] = [];
+
+    render(<ParsedResults results={results} />);
+
+    // Check if the component renders a message when there are no results
+    expect(screen.getByText("Parsed Results:")).toBeInTheDocument();
+    expect(screen.getByText("No results to display.")).toBeInTheDocument();
+  });
+});
--- src/components/ParseResult/ParsedResults.tsx
+++ src/components/ParseResult/ParsedResults.tsx
@@ -7,6 +7,7 @@ interface Props {
 export const ParsedResults: React.FC<Props> = ({ results }) => (
   <div>
     <h2>Parsed Results:</h2>
+    {results.length === 0 && <div>No results to display.</div>}
     {results.map((result) => (
       <div key={result.lineNumber}>
         Send {result.amount} to address {result.address}
